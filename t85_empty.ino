906ba91aa2c0f7a0ef0c4295f53fb5bd69a59d8b
pcfreak1201 <68873801+pcfreak1201@users.noreply.github.com>
2020-09-29 07:23:50 +0200

    Core-less Demo

---
+++ t85_empty.ino
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+
+int led = 2; // In port B
+
+void setup() {                
+ DDRB = DDRB | 1<<led;         // Define PB5 as an output
+}
+
+volatile long Counter;
+
+void delay (long n) {          // Delay by n milliseconds
+ Counter = 469 * n;
+  do Counter--; while (Counter != 0);
+}
+
+// The loop routine runs over and over again forever:
+void loop() {
+  PORTB = PORTB | 1<<led;      // Take PB5 high
+  delay(100);                 // Wait for a second
+  PORTB = PORTB & ~(1<<led);   // Take PB5 low
+  delay(100);                 // Wait for a second
+}
+
+int main() {
+  sei();
+  setup();
+  for(;;)
+    loop();
+}
